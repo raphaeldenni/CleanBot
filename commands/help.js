64112634f81902e366de33315e9cf521549a4099
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2021-08-18 00:03:54 +0200

    Base du bot + Commandes de base

---
+++ commands/help.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'help',
+    description: 'Affiche un liste des commandes disponibles',
+    execute(message) {
+        message.delete({timeout: 1});
+        message.reply({embed : {
+            color: 0x00ffff,
+            description: '📜 Une liste des commandes disponibles vous a été envoyé en message privé',
+        }})
+        message.author.createDM().then(channel => {
+            channel.send({embed : {
+                color: 0x00ffff,
+                title: `\n__Salut ${message.author.username} 👋, les commandes suivantes sont disponibles pour CleanBot :__\n\n`,
+                description: '**_help** [*Affiche un liste des commandes disponibles et leurs effets*]\n**_ping** [*Affiche la latence du bot en ms*])',
+            }})
+        })
+    }
+}
