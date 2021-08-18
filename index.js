4ff3ed7382a97cd6631e4e9749ca8ba913f42c6a
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2021-08-18 17:50:43 +0200

    Update welcome.js & index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ client.on("ready", () =>{
   client.user.setPresence({
       status: "online",
       activity: {
-          name: `la liste ${prefix}help`,
+          name: `${prefix}help et cleanwalk.org`,
           type: "WATCHING"
   }})
 })
--- modules/welcome.js
+++ modules/welcome.js
@@ -19,7 +19,7 @@ const applyText = (canvas, text) => {
 };
 
 client.on('guildMemberAdd', async member => {
-	const channel = member.guild.channels.cache.find(ch => ch.name === 'général');
+	const channel = member.guild.channels.cache.find(ch => ch.name === 'test-1');
 	if (!channel) return;
 
 	const canvas = Canvas.createCanvas(700, 250);
