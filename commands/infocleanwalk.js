fcd443bf93529524764a0429dd62cde57289f8bd
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2023-02-14 16:47:18 +0100

    Delete Pupeeter package and use https requests directly

--- commands/infocleanwalk.js
+++ commands/infocleanwalk.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const moment = require('moment');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -6,83 +7,80 @@ module.exports = {
 		.setDescription('Affiche les cinq prochaines cleanwalks'),
 
 	async execute(interaction, message) {
-        const puppeteer = require("puppeteer");
-    
-        try {
-          const browser = await puppeteer.launch();
-          const page = await browser.newPage();
-          await page.goto('https://www.cleanwalk.org/cleanwalks?page=1', { waitUntil: 'networkidle0' });
-    
-          const scrap = await page.evaluate(() => {
-    
-            list = [];
-    
-            for (i = 0; i < 5; i++) {
-              try {
-              let url = document.getElementsByClassName('media')[i].href.trim();
-              let title = document.getElementsByClassName('title is-4')[i].innerHTML.trim()
-              /*
-              let elements = document.querySelectorAll('.flex items-center')[i];
-              console.log(elements)
-              let location = elements[i].innerHTML.trim();
-              let datetime = elements[i+1].innerHTML.trim();
-              */
-              let user_url = document.getElementsByClassName('link flex-column items-center justify-center has-text-centered')[i].href.trim();
-              let user_name = document.getElementsByClassName('m-a-none')[i].innerHTML.trim();
-              let user_avatar = document.getElementsByClassName('is-rounded')[i].src.trim();
-    
-              list.push(url, title, /*location, datetime,*/ user_url, user_name, user_avatar);
-              } catch (err) {
-                console.error(err);
-              }
-    
-            };
-            return list;
-            
-          });
-    
-          console.log(scrap);
-    
-          await browser.close();
-          
-          if (scrap.length != 0) {
-              await interaction.reply({embed : {
-              color : '#2140b6',
-              title : 'Voici les cinq prochaines cleanwalk',
-              author : {url : 'https://cleanwalk.org', name : 'Cleanwalk.org'},
-              thumbnail : {url : 'https://i.imgur.com/byqdKyp.png'},
-            }});
-    
-            for (i = 0; i < scrap.length; ) {
-              await new Promise(r => setTimeout(r, 500));
-              await interaction.reply({embed : {
-                color : '#2140b6',
-                url : scrap[i],
-                title : scrap[i+1],
-                //description : 'Localisation : ' + scrap[i+2] + '\nDate et heure : ' + scrap[i+3],
-                author : {url : scrap[i+2], name : scrap[i+3], iconURL : scrap[i+4]},
-              }});
-              i += 5;
-            };
-    
-          } else {
-            await interaction.reply({embed : {
-              color : '#ff0000',
-              title : 'Aucune cleanwalk disponible',
-              author : {url : 'https://cleanwalk.org', name : 'Cleanwalk.org'},
-              thumbnail : {url : 'https://i.imgur.com/byqdKyp.png'},
-            }});
-    
-          };
-    
-        } catch (err) {
-          console.log(err);
-    
-        };
+    // Get cleanwalks data
+    let cleanwalks = await fetch('https://cleanwalk-api.herokuapp.com/v1/cleanwalks')
+      .then(response => response.json())
+      .then(data => data);
 
-	},
+    // Reply data with embed
+    await interaction.reply({ embeds:
+      [{	
+          color: 0x2140b6,
+          title: `\n🍃 Voici les 5 prochaines cleanwalks :\n\n`,
+
+          fields: [
+            {
+              name: cleanwalks[0].name,
+              value: `**Date :** ${moment.parseZone(cleanwalks[0].date).format("DD-MMM-YY HH:m:s")}\n
+              **Lieu :** ${cleanwalks[0].location}\n
+              **Maps :** https://www.google.com/maps/@${cleanwalks[0].lat},${cleanwalks[0].lng},15z\n
+              **Participants :** ${cleanwalks[0].number_walker}/${cleanwalks[0].number_max_walker}\n
+              **Organisateur :** ${cleanwalks[0].leader.name}\n`,
+            },
+            {
+              name: cleanwalks[1].name,
+              value: `**Date :** ${moment.parseZone(cleanwalks[1].date).format("DD-MMM-YY HH:m:s")}\n
+              **Lieu :** ${cleanwalks[1].location}\n
+              **Maps :** https://www.google.com/maps/@${cleanwalks[1].lat},${cleanwalks[1].lng},15z\n
+              **Participants :** ${cleanwalks[1].number_walker}/${cleanwalks[1].number_max_walker}\n
+              **Organisateur :** ${cleanwalks[1].leader.name}\n`,
+            },
+            {
+              name: cleanwalks[2].name,
+              value: `**Date :** ${moment.parseZone(cleanwalks[2].date).format("DD-MMM-YY HH:m:s")}\n
+              **Lieu :** ${cleanwalks[2].location}\n
+              **Maps :** https://www.google.com/maps/@${cleanwalks[2].lat},${cleanwalks[2].lng},15z\n
+              **Participants :** ${cleanwalks[2].number_walker}/${cleanwalks[2].number_max_walker}\n
+              **Organisateur :** ${cleanwalks[2].leader.name}\n`,
+            },
+            {
+              name: cleanwalks[3].name,
+              value: `**Date :** ${moment.parseZone(cleanwalks[3].date).format("DD-MMM-YY HH:m:s")}\n
+              **Lieu :** ${cleanwalks[3].location}\n
+              **Maps :** https://www.google.com/maps/@${cleanwalks[3].lat},${cleanwalks[3].lng},15z\n
+              **Participants :** ${cleanwalks[3].number_walker}/${cleanwalks[3].number_max_walker}\n
+              **Organisateur :** ${cleanwalks[3].leader.name}\n`,
+            },
+            {
+              name: cleanwalks[4].name,
+              value: `**Date :** ${moment.parseZone(cleanwalks[4].date).format("DD-MMM-YY HH:m:s")}\n
+              **Lieu :** ${cleanwalks[4].location}\n
+              **Maps :** https://www.google.com/maps/@${cleanwalks[4].lat},${cleanwalks[4].lng},15z\n
+              **Participants :** ${cleanwalks[4].number_walker}/${cleanwalks[4].number_max_walker}\n
+              **Organisateur :** ${cleanwalks[4].leader.name}\n`,
+            },
+            {
+              name: 'Plus d\'infos :',
+              value: 'https://cleanwalk.org',
+            },
+          ],
+
+          thumbnail: {
+            url: 'https://i.imgur.com/byqdKyp.png',
+          },
+
+          footer: {
+            text: 'Cleanwalk.org',
+            icon_url: 'https://i.imgur.com/byqdKyp.png',
+            link: 'https://cleanwalk.org',
+          },
+          timestamp: new Date(),
+      }]
+    });
+}};
     
-};
+
+//https://www.google.com/maps/@(latitude),(longitude),15z
 
 /*
 Copyright 2021-2022 Raphaël DENNI & Cleanwalk.org
